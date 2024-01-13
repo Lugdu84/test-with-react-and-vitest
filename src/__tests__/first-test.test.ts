b24ff84dfba5ff66791d3bff6fe9b1ae715408d6
David Grammatico <david.lugdunum@gmail.com>
2024-01-13 18:06:47 +0100

    install and configure project

---
+++ src/__tests__/first-test.test.ts
@@ -0,0 +1,9 @@
+describe('First test', () => {
+	it('should pass', () => {
+		expect(true).toBe(true);
+	});
+	test('should pass', () => {
+		console.log('test');
+		expect(true).toBe(true);
+	});
+});
