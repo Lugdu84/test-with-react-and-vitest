786c186b41db207aa7fb0015dc1486c68ca0ff03
David Grammatico <david.lugdunum@gmail.com>
2024-01-17 14:04:53 +0100

    add SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Counter from './components/counter';
+import SearchBar from './components/search-bar';
 
 function App() {
 	return (
 		<div className="container p-4">
 			<Counter />
+			<SearchBar onSearch={(e) => console.log(e)} />
 		</div>
 	);
 }
